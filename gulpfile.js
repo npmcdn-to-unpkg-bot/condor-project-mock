d78d235d06c0a94769ebbc4d7c9d53fe653649cc
Carlos Frias <carlos.a.frias@gmail.com>
2016-08-23 15:20:39 -0300

    Change architecture to GULP

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+		pug = require('gulp-pug'),
+		browserSync = require('browser-sync').create(),
+		testData = require('./testData.js');
+
+gulp.task('serve', ['template-compile'], function serve() {
+	browserSync.init({
+		server: './build'
+	});
+
+	gulp.watch('src/assets/*.css', ['style-compile']);
+	gulp.watch('src/assets/*.js');
+	gulp.watch('src/*.pug', ['template-compile']).on('change', browserSync.reload);
+});
+
+gulp.task('template-compile', function pugCompile() {
+	return gulp.src('src/pages/*.pug')
+		.pipe(pug({
+			locals: testData
+		}))
+		.pipe(gulp.dest('build/'))
+	;
+});
+
+gulp.task('style-compile', function styleCompile() {
+	return gulp.src('src/assets/styles/*.css')
+		.pipe(gulp.dest('build/styles'))
+	;
+});
+
+gulp.task('js-compile', function jsCompile() {
+	return gulp.src('src/assets/js/*.js')
+		.pipe(gulp.dest('build/js'))
+	;
+});
---
+++ src/assets/js/index.js
@@ -0,0 +1,6 @@
+(function(){
+	document.addEventListener('DOMContentLoaded', function() {
+		var body = document.querySelector('body');
+		body.innerHTML += '<br>Text Added with JS.';
+	});
+})();
