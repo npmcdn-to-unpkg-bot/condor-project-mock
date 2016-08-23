957e2899a5902c1de79fe4eac6a9e9a051354166
Carlos Frias <carlos.a.frias@gmail.com>
2016-08-23 18:16:29 -0300

    Fix bug in index

--- testData.js
+++ testData.js
@@ -30,6 +30,30 @@ var featured = [
 		excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
 		'Praesent tristique vestibulum justo id feugiat. Nam consequat, nibh' +
 		' sit amet volutpat consectetur, nibh metus rhoncus massa, id blandit mauris arcu vitae metus.'
+	},
+	{
+		title: 'Nuevos Lorem Ipsum',
+		author: 'Carlos Frias',
+		date: '10/09/2016',
+		excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+		'Praesent tristique vestibulum justo id feugiat. Nam consequat, nibh' +
+		' sit amet volutpat consectetur, nibh metus rhoncus massa, id blandit mauris arcu vitae metus.'
+	},
+	{
+		title: 'Otros Lorem Ipsum',
+		author: 'Carlos Frias',
+		date: '10/10/2016',
+		excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+		'Praesent tristique vestibulum justo id feugiat. Nam consequat, nibh' +
+		' sit amet volutpat consectetur, nibh metus rhoncus massa, id blandit mauris arcu vitae metus.'
+	},
+	{
+		title: 'Tentios Lorem Ipsum',
+		author: 'Carlos Frias',
+		date: '24/09/2016',
+		excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+		'Praesent tristique vestibulum justo id feugiat. Nam consequat, nibh' +
+		' sit amet volutpat consectetur, nibh metus rhoncus massa, id blandit mauris arcu vitae metus.'
 	}
 ];
 
