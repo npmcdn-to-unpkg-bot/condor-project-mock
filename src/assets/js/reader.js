82b1df0ea4e311c60ac535b4e4efc0be9067d19f
Carlos Frias <carlos.a.frias@gmail.com>
2016-08-26 16:25:17 -0300

    Add hidable toolbar

--- src/assets/js/reader.js
+++ src/assets/js/reader.js
@@ -5,11 +5,20 @@ var _TOOLBAR = (function ToolbarInstanciator() {
 	var instance;
 
 	function Toolbar() {
+		var headerRef = document.getElementsByClassName('top-header')[0];
+		var footerRef = document.getElementsByClassName('top-footer')[0];
+
+		var mainRef = document.getElementsByTagName('main')[0];
+
 		var toolIconRef = document.getElementById('tools-icon');
 
 		var toolBarRef = document.getElementById('tool-bar');
 
+		var articleRef = document.getElementsByTagName('article')[0];
+
 		var collapsed = true;
+
+		var hidden = false;
 
 		var toggle = function toggleToolbar(evt) {
 			if(collapsed) {
@@ -25,10 +34,28 @@ var _TOOLBAR = (function ToolbarInstanciator() {
 			}
 		};
 
-		// X 
+		var hide =  function hideToolbar() {
+			if(hidden) {
+				mainRef.classList.remove('no-nav');
+				headerRef.classList.add('flex');
+				headerRef.classList.remove('dn');
+				footerRef.classList.add('flex');
+				footerRef.classList.remove('dn');
+				hidden = false;
+			} else {
+				mainRef.classList.add('no-nav');
+				headerRef.classList.remove('flex');
+				headerRef.classList.add('dn');
+				footerRef.classList.remove('flex');
+				footerRef.classList.add('dn');
+				hidden = true;
+			}
+		};
+
 		return {
 			init: function initToolbar() {
 				toolIconRef.addEventListener('click', toggle);
+				articleRef.addEventListener('click', hide);
 			}
 		};
 	}
